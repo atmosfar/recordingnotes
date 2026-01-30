afe9664e331fbfee9f170eeb1832397302682b95
atmosfar <atmosfar@doublejump.eu>
2026-01-30 21:40:39 +0000

    feat(auth): Set session expiration to 24 hours

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(session({
   secret: process.env.SESSION_SECRET || 'fallback_secret_for_dev_only',
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false } // Set to true if using HTTPS
+  cookie: { 
+    secure: false, // Set to true if using HTTPS
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }
 }));
 
 /**
